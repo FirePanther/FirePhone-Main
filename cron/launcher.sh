9b745b0280834ff41960339d65462155e04e8ae6
FirePanther <git@suat.be>
2015-06-05 03:21:39 +0200

    added telegram to launcher

--- cron/launcher.sh
+++ cron/launcher.sh
@@ -7,7 +7,7 @@ if [ "$(date +%k)" -ge 2 ] && [ "$(date +%k)" -le 6 ]; then
 	# if FireTime app is running
 	if [ -n "$(launchctl list | grep pro.firepanther.FireTime)" ]; then
 		# just to be sure, vain glory is not running :D
-		if [ -z "$(launchctl list | grep com.superevilmegacorp.vainglory)" ]; then
+		if [ -z "$(launchctl list | grep com.superevilmegacorp)" ]; then
 			# if last run older than 14 hours
 			if [ ! -f /tmp/last-launch.txt ] || [ "$(cat /tmp/last-launch.txt)" -lt "$(expr $(date +%s) - 3600 \* 12)" ]; then
 				# if charging
@@ -16,6 +16,7 @@ if [ "$(date +%k)" -ge 2 ] && [ "$(date +%k)" -le 6 ]; then
 					sblaunch -b com.reederapp.rkit2.ios
 					sblaunch -b com.omnigroup.OmniFocus2.iPhone
 					sblaunch -b com.ifttt.ifttt
+					sblaunch -b ph.telegra.Telegraph
 					sleep 3
 					open pro.firepanther.FireTime
 					date +%s > /tmp/last-launch.txt
