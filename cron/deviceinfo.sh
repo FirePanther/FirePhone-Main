bb73546c4c8c6352c64888f1b5b1abba55fcdedc
FirePanther <git@suat.be>
2015-06-10 03:50:45 +0200

    try sleep

--- cron/deviceinfo.sh
+++ cron/deviceinfo.sh
@@ -3,6 +3,7 @@
 # this script sends the battery status to the server
 
 # battery
+sleep 1
 battery=$(sbdevice -l)
 charging=$(if [ "`sbdevice -s`" = "Charging" ]; then echo "c"; else echo ""; fi)
 if [ ! -f /tmp/info-battery.txt ] || [ "$(cat /tmp/info-battery.txt)" != "$charging$battery" ]; then
