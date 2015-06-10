5e5d8ee5811a4dcdfc31f85bcf2cfaad52eb7665
FirePanther <git@suat.be>
2015-06-10 02:36:27 +0200

    fixed battery info

--- cron/deviceinfo.sh
+++ cron/deviceinfo.sh
@@ -6,5 +6,5 @@
 battery=$(sbdevice -l)
 if [ ! -f /tmp/info-battery.txt ] || [ "$(cat /tmp/info-battery.txt)" != "$battery" ]; then
 	echo $battery > /tmp/info-battery.txt
-	wget -qO- "http://suat.be/api/ios/deviceinfo?s=b&v=$battery" &> /dev/null
+	wget -qO- "http://suat.be/api/ios/deviceinfo.php?s=b&v=$battery" &> /dev/null
 fi
