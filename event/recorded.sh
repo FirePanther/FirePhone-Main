a5cdb2a904fbd42b3af07458ea6fd80eda652ac1
FirePanther <git@suat.be>
2015-06-17 03:50:54 +0200

    Fixing and debugger prints

--- event/recorded.sh
+++ event/recorded.sh
@@ -2,23 +2,33 @@
 drec=/var/mobile/Library/Keyboard/DisplayRecorder
 recs=/User/Documents/Recordings
 
+sleep 3
+
 for i in {1..50}
 do
 	last=$(ls -t | grep -i .mov | head -1)
 	if [ -z $last ]; then
+		echo "Waiting..."
 		sleep .1
 	else
+		echo "Breaking wait"
 		break
 	fi
 done
 if [ -n $last ]; then
-	value=$(sbalert -t "Recording" -m "Do you want to rename the recording?" -d "Rename" -a "No" -p)
+	value=$(sbalert -t "Recording" -m "Do you want to rename the recording?" -d "Rename" -a "Just move" -o "Cancel" -p)
+	button=$?
 	date=$(date +%Y-%m-%d_%H.%M)
-	if [ $? = 0 ]; then
+	echo "Button $button pressed"
+	if [ $button = 0 ]; then
 		# clicked on "Rename"
-		mv $drec/$last $recs/$date-$value.mov
-	else
-		# cancelled
-		mv $drec/$last $recs/$date-$last
+		echo "Renaming $last to $value.mov"
+		mv "$drec/$last" $recs/$date-$value.mov
+	elif [ $button = 1]; then
+		# just move
+		echo "Moving $last"
+		mv "$drec/$last" $recs/$date-$last
 	fi
+else
+	echo "Couldn't find *.mov"
 fi
