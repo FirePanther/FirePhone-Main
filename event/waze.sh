b27bb522bd7741dc47e9bd3db670d63f7b549f7f
FireBook <git@suat.be>
2015-08-09 00:48:55 +0200

    Waze bluetooth check

--- event/waze.sh
+++ event/waze.sh
@@ -2,4 +2,6 @@
 
 # dependency: activator, bypass, noslowanimations
 
-activator send com.bd452.bypass && sleep 1 && sblaunch com.waze.iphone
+if [ -f "/tmp/bt.txt" ] && [ "$(cat /tmp/bt.txt)" == "1" ]; then
+	activator send com.bd452.bypass && sleep 1 && sblaunch com.waze.iphone
+fi
