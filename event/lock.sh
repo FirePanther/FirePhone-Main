c46bd9e7ad16a68f4cbd5267626e9dbc103e6dd3
FireBook <git@suat.be>
2015-08-09 04:51:47 +0200

    renamed tmp file to *lock*

--- event/lock.sh
+++ event/lock.sh
@@ -2,7 +2,7 @@
 
 # lock the device again (@see unlock.sh)
 
-if [ -f "/tmp/_fp-unlock.txt" ]; then
-	rm /tmp/_fp-unlock.txt
+if [ -f "/tmp/_fp-lock.txt" ]; then
+	rm /tmp/_fp-lock.txt
 	activator send libactivator.system.sleepbutton
 fi
