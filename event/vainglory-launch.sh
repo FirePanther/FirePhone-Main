0f36be6f9d34874106855c0aea5694ba8d1e9f40
FireBook <git@suat.be>
2015-08-15 02:36:52 +0200

    logger included in vainglory

--- event/vainglory-launch.sh
+++ event/vainglory-launch.sh
@@ -2,6 +2,8 @@
 
 # dependency: activator, displayrecorder
 
+log launched vainglory
+
 record=1
 
 # wait/check 15x3 seconds (if vainglory was launched by accident)
@@ -10,12 +12,14 @@ do
 	sleep 15
 	# if vainglory was closed: don't record
 	if [ "$(activator current-app)" != "com.superevilmegacorp.kindred" ]; then
+		log vainglory quitted
 		record=0
 		break
 	fi
 done
 
 if [ "$record" == "1" ]; then
+	log recording vainglory, switch on dnd + displayrecorder
 	activator send switch-on.com.a3tweaks.switch.do-not-disturb
 	activator send switch-on.com.rpetrich.displayrecorder
 fi
--- event/vainglory-quit.sh
+++ event/vainglory-quit.sh
@@ -2,12 +2,17 @@
 
 # dependency: activator, displayrecorder
 
+log quitted vainglory
+
 # only turn off dnd if it's between 7 and 0 o'clock
 hour=$(date +%H)
+log Hour is $hour
 if [ $hour -gt 6 ]; then
+	log (-gt 6), switch off dnd
 	activator send switch-off.com.a3tweaks.switch.do-not-disturb
 fi
 
 # finish recording and offer rename
+log switch off displayrecorder
 activator send switch-off.com.rpetrich.displayrecorder
 /firephone/event/recorded.sh
