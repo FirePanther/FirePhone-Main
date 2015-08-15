52f589b53b17f17b29796013072d2d010150bcd4
FireBook <git@suat.be>
2015-08-15 16:39:13 +0200

    again space bug

--- Sync/inc/log.sh
+++ Sync/inc/log.sh
@@ -4,6 +4,6 @@ log() {
 	index=$(expr $(date +%d) + $(date +%m) + $(date +%y))
 	time=$(date +%H.%M)
 	logfile="/firephone/log/${index}_${time} - $(basename /${0%.*}).log"
-	if [[ ! -f "$logfile" ]]; then printf "$0\nPWD: $PWD\n" > $logfile; fi
+	if [[ ! -f "$logfile" ]]; then printf "$0\nPWD: $PWD\n" > "$logfile"; fi
 	printf ">>> $1\n" >> "$logfile"
 }
