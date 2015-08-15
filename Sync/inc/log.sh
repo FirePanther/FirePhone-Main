e8e3bb168a23756aeba9d3889ef734e6200e0c26
FireBook <git@suat.be>
2015-08-15 16:44:01 +0200

    fixed index

--- Sync/inc/log.sh
+++ Sync/inc/log.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 log() {
 	if [ ! -d /firephone/log ]; then mkdir /firephone/log; fi
-	index=$(expr $(date +%d) + $(date +%m) + $(date +%y))
+	index=$(($(date +%-d) + $(date +%-m) * 12))
 	time=$(date +%H.%M)
 	logfile="/firephone/log/${index}_${time} - $(basename /${0%.*}).log"
 	if [[ ! -f "$logfile" ]]; then printf "$0\nPWD: $PWD\n" > "$logfile"; fi
