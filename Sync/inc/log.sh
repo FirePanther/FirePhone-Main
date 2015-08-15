6df6769fdf3394dfdd6daca6a4803a766c25925b
FireBook <git@suat.be>
2015-08-15 16:37:21 +0200

    variable bug

--- Sync/inc/log.sh
+++ Sync/inc/log.sh
@@ -3,7 +3,7 @@ log() {
 	if [ ! -d /firephone/log ]; then mkdir /firephone/log; fi
 	index=$(expr $(date +%d) + $(date +%m) + $(date +%y))
 	time=$(date +%H.%M)
-	logfile="/firephone/log/$index_$time - $(basename /${0%.*}).log"
+	logfile="/firephone/log/${index}_${time} - $(basename /${0%.*}).log"
 	if [[ ! -f $logfile ]]; then printf "$0\nPWD: $PWD\n" > $logfile; fi
 	printf ">>> $1\n" >> $logfile
 }
