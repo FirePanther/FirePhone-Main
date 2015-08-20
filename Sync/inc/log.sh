805cff711f9c0e10a71835dc7db234d9aa4369b6
FireBook <git@suat.be>
2015-08-20 16:20:05 +0200

    logfile function

--- Sync/inc/log.sh
+++ Sync/inc/log.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 log() {
+	logfile="$(logfile)"
+	if [[ ! -f "$logfile" ]]; then printf "$0\nPWD: $PWD\n" > "$logfile"; fi
+	printf ">>> $1\n" >> "$logfile"
+}
+
+logfile() {
 	if [ ! -d /firephone/log ]; then mkdir /firephone/log; fi
 	index=$(($(date +%-d) + $(date +%-m) * 12))
 	time=$(date +%H.%M)
 	logfile="/firephone/log/${index}_${time} - $(basename /${0%.*}).log"
-	if [[ ! -f "$logfile" ]]; then printf "$0\nPWD: $PWD\n" > "$logfile"; fi
-	printf ">>> $1\n" >> "$logfile"
+	echo $logfile
 }
