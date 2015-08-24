89317e0bb4f4b603c4c40628c21258beb0bea0a5
FireBook <git@suat.be>
2015-08-24 17:50:32 +0200

    deviceinfo is now password protected

---
+++ Sync/inc/deviceinfoPassword.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+deviceinfoPassword=2918
--- cron/deviceinfo.sh
+++ cron/deviceinfo.sh
@@ -3,6 +3,7 @@
 # dependency: sbutils, wget
 
 # this script sends the battery status to the server
+/firephone/Sync/inc/deviceinfoPassword.sh
 
 # battery
 sleep 1
@@ -10,5 +11,5 @@ battery=$(sbdevice -l)
 charging=$(if [ "`sbdevice -s`" = "Charging" ]; then echo "c"; else echo ""; fi)
 if [ ! -f /tmp/_fp-info-battery.txt ] || [ "$(cat /tmp/_fp-info-battery.txt)" != "$charging$battery" ]; then
 	echo "$charging$battery" > /tmp/_fp-info-battery.txt && chown mobile /tmp/_fp-info-battery.txt
-	wget -qO- "http://suat.be/api/ios/deviceinfo.php?s=b&v=$battery&$charging" &> /dev/null
+	wget -qO- "http://suat.be/api/ios/deviceinfo.php?p=$deviceinfoPassword&s=b&v=$battery&$charging" &> /dev/null
 fi
