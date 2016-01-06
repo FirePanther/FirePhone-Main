7cbb7cb0a647b2f8309b96589a2d0c6be4b3f37e
FireBook <git@suat.be>
2016-01-07 00:22:59 +0100

    Disable FireTime

--- event/charge.sh
+++ event/charge.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-/firephone/event/unlock.sh
+#/firephone/event/unlock.sh
 
-activator send pro.firepanther.FireTime
+#activator send pro.firepanther.FireTime
 
-activator send switch-off.com.a3tweaks.switch.autolock
+#activator send switch-off.com.a3tweaks.switch.autolock
 
 /firephone/cron/deviceinfo.sh
--- event/unplug.sh
+++ event/unplug.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ "$(activator current-app)" == "pro.firepanther.FireTime" ]; then
-	killall FireTime
-	/firephone/event/lock.sh
-fi
+#if [ "$(activator current-app)" == "pro.firepanther.FireTime" ]; then
+#	killall FireTime
+#	/firephone/event/lock.sh
+#fi
 
-activator send switch-on.com.a3tweaks.switch.autolock
+#activator send switch-on.com.a3tweaks.switch.autolock
 
 /firephone/cron/deviceinfo.sh
