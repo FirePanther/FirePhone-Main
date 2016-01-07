0ba33eab0edfc7a881b05d9c6d7b2b9de57274a9
FirePhone <git@suat.be>
2016-01-07 14:21:41 +0100

    lower case cydia names

--- cron/backup.sh
+++ cron/backup.sh
@@ -2,13 +2,13 @@
 
 # dependency: git, awk (gawk)
 
-cd /firephone/Cydia
+cd /firephone/cydia
 
 # cydia packages
-cat /var/lib/dpkg/status | grep Package: | awk -F: '{print $2}' | sort > Packages.txt
+cat /var/lib/dpkg/status | grep Package: | awk -F: '{print $2}' | sort > packages
 
 # cydia sources
-cat /etc/apt/sources.list.d/cydia.list > Sources.txt
+cat /etc/apt/sources.list.d/cydia.list > sources
 
 # git
 git add .
