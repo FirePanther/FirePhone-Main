ba345d28a4850c2857c5d08f57031eb8d9483f00
FirePanther <git@suat.be>
2016-03-22 01:59:59 +0100

    Launch Google Photos and URL schemes
    No FireTime anymore, no Dropbox (no photo sync), just use Google Ohotos to sync (and iCloud Photo Library). Use URL schemes (for Tweetbot and Todoist).

--- cron/launcher.sh
+++ cron/launcher.sh
@@ -5,16 +5,11 @@
 # launch some apps in the night to sync some stuff
 
 currentMode=$(activator current-mode)
-firetime=0
 
 # if between 0 and 11 am
 if [ "$(date +%k)" -ge 0 ] && [ "$(date +%k)" -le 8 ]; then
-
-	# if FireTime app is running
-	if [ "$(activator current-app)" == "pro.firepanther.FireTime" ]; then firetime=1; fi
-	
-	# if not in an (important) app (like vainglory :D)
-	if [ "$firetime" == "1" ] || [ "$currentMode" == "lockscreen" ]; then
+	# if not in an (important) app
+	if [ "$currentMode" == "lockscreen" ]; then
 		
 		# if last run older than 14 hours
 		if [ ! -f /tmp/_fp-last-launch.txt ] || [ "$(cat /tmp/_fp-last-launch.txt)" -lt "$(expr $(date +%s) - 3600 \* 12)" ]; then
@@ -25,19 +20,18 @@ if [ "$(date +%k)" -ge 0 ] && [ "$(date +%k)" -le 8 ]; then
 				
 				activator send com.reederapp.rkit2.ios && sleep 2
 				activator send ph.telegra.Telegraph && sleep 2
-				activator send com.tapbots.Tweetbot4 && sleep 4
-				activator send com.getdropbox.Dropbox && sleep 4
+				
+				# tweetbot api: http://tapbots.net/tweetbot3/support/url-schemes/
+				uiopen tweetbot://FirePanther/timeline & sleep 4
+				
+				#activator send com.getdropbox.Dropbox && sleep 4
 				activator send com.airmailapp.iphone && sleep 4
-				activator send com.todoist.ios && sleep 4
 				
-				if [ "$firetime" == "1" ]; then
-					activator send pro.firepanther.FireTime && sleep 2
+				activator send com.google.photos && sleep 4
 				
-				# don't use homebutton anymore, let todoist open for me for the next day :)
-				# else
-					#activator send libactivator.system.homebutton && sleep 1
-					
-				fi
+				# todoist api: https://github.com/Doist/todoist-api/blob/master/source/includes/_urlschemes.md
+				uiopen todoist://today && sleep 4
+				
 				date +%s > /tmp/_fp-last-launch.txt
 			fi
 		fi
