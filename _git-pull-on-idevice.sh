6f99100263d3ff6432a9ad1eb3d25953bbbe3331
FirePanther <git@suat.be>
2016-03-22 02:05:58 +0100

    Extended and commented

--- _git-pull-on-idevice.sh
+++ _git-pull-on-idevice.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-ssh root@firephone "cd /firephone; git pull origin master"
+# execute on iMac/MacBook, git pulls on iDevice and sets execute permissions
+ssh root@firephone "cd /firephone; git pull --rebase origin master && chmod -R +x /firephone/event/ && chmod -R +x /firephone/cron/"
